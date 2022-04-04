d8fb0b2ca68365271bf78d582c741041a60f97e5
Oskari Rautiainen <orautiainen@splunk.com>
2022-04-03 17:04:15 -0700

    Cleanup utilitie

--- cmd/words-api/main.go
+++ cmd/words-api/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
-	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -25,54 +22,35 @@ type Word struct {
 }
 
 
-func readCsvFile(filePath string) [][]string {
-	f, err := os.Open(filePath)
-	if err != nil {
-			log.Fatal("Unable to read input file " + filePath, err)
-	}
-	defer f.Close()
-
-	csvReader := csv.NewReader(f)
-	records, err := csvReader.ReadAll()
-	if err != nil {
-			log.Fatal("Unable to parse file as CSV for " + filePath, err)
-	}
-
-	return records
-}
-
-func readWordsTxtFile(filePath string) []string {
-	file, err := os.Open(filePath)
-	if err != nil {
-			log.Fatal(err)
-	}
-	defer func() {
-			if err = file.Close(); err != nil {
-					log.Fatal(err)
-			}
-	}()
-	rawWords, err := ioutil.ReadAll(file)
-	words := strings.Split(string(rawWords), "\n")
-	return words
-
-}
 
 func main() {
 	// Seeding to randomize words by time
 	rand.Seed(time.Now().UTC().UnixNano())
 	// Read the main words file
-	words := readWordsTxtFile("../words-api/assets/words.txt")
+	// TODO: Create a map of word: <Word> for easier lookups later
+	words := ReadTxtFileByLine("../../assets/words.txt")
 	// Read frequency records
-	mostFreqWords := readCsvFile("../words-api/assets/unigram_freq.csv")
+	// TODO: Convert frequency count to a frequency score and return in the json data
+	mostFreqWords := ReadCsvFile("../../assets/unigram_freq.csv")
+	// Sort by the frequency count
 	sort.Slice(mostFreqWords, func(i, j int) bool {
 		r1, _ := strconv.Atoi(mostFreqWords[i][1])
 		r2, _ :=  strconv.Atoi(mostFreqWords[j][1])
 		return r1 > r2
 	})
 
+
+	/**
+	 	* Return info about a specific word
+		*    /word/<:word>
+		* 
+		* Query Params: 
+		* 	length=<int> : number of characters the random word should contain
+		*/
 	wordHandler := func(w http.ResponseWriter, req *http.Request) {
     vars := mux.Vars(req)
 		word := vars["word"]
+		// Iterate through all known words and look for the matching one
 		for _, curWord := range words {
 			if curWord == word {
 				result := Word{curWord, 0}
@@ -82,26 +60,36 @@ func main() {
 				return
 			}
 		}
+		// If there was no match above then it is an unknown word
 		w.WriteHeader(http.StatusBadRequest)
 		http.Error(w, "None Found", http.StatusBadRequest)
 		return
 	}
 
 	wordsHandler := func(w http.ResponseWriter, req *http.Request) {
+		// TODO: Return in an array of objects format
 		io.WriteString(w, strings.Join(words[:], "\n"))
 		return
 	}
 
+	/**
+	 	* Return a single randomized word.
+		* 
+		* Query Params: 
+		* 	length=<int> : number of characters the random word should contain
+		*/
 	randomWordHandler := func(w http.ResponseWriter, req *http.Request) {
 		req.ParseForm()
 		_, hasLength := req.Form["length"]
 		length, _ := strconv.Atoi(req.URL.Query().Get("length"))
 
-		// Get the top 10,000 most common words for the length
+		// Use the top X number of most common words
+		// The higher the number the higher the chance of returning uncommon words
 		wordLimit := 1000
 		wordsToChoose := make([]string, wordLimit)	
     i := 0
 		added := 0
+		// Calculate the most frequent words that match the length (if provided)
 		for added < wordLimit && i < len(mostFreqWords) {
 			if (hasLength) {
 				if (len(mostFreqWords[i][0]) == length) {
@@ -114,7 +102,7 @@ func main() {
 			}
 			i++
 		}
-		// Choose a word at random from the most frequent list
+		// Choose a word at random from the most frequent sub-list
 		randIdx := rand.Intn(wordLimit)
 		randomWord := wordsToChoose[randIdx]
 		result := Word{randomWord, 0}
@@ -127,14 +115,19 @@ func main() {
 		return
 	}
 	
-	
+	// Router setup
 	router := mux.NewRouter().StrictSlash(true)
+
 	router.HandleFunc("/random", randomWordHandler).Methods("GET","OPTIONS")
-		log.Println("Listing for requests at http://localhost:9001/random")
+	log.Println("Listening for requests at http://localhost:9001/random")
+
 	router.HandleFunc("/word/{word}", wordHandler).Methods("GET","OPTIONS")
-		log.Println("Listing for requests at http://localhost:9001/word")
-	router.HandleFunc("/words", wordsHandler).Methods("GET","OPTIONS")
-		log.Println("Listing for requests at http://localhost:9001/words")
+	log.Println("Listening for requests at http://localhost:9001/word")
+
+	router.HandleFunc("/word", wordsHandler).Methods("GET","OPTIONS")
+	log.Println("Listening for requests at http://localhost:9001/words")
+
+	// TODO: Return a API doc page w/ examples like type ahead
 	http.Handle("/", router)
 	http.ListenAndServe(":9001", handlers.CORS()(router))
 }
---
+++ cmd/words-api/utils.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/csv"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+func ReadCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+			log.Fatal("Unable to read input file " + filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+			log.Fatal("Unable to parse file as CSV for " + filePath, err)
+	}
+	return records
+}
+
+func ReadTxtFileByLine(filePath string) []string {
+	file, err := os.Open(filePath)
+	if err != nil {
+			log.Fatal(err)
+	}
+	defer func() {
+			if err = file.Close(); err != nil {
+					log.Fatal(err)
+			}
+	}()
+	rawWords, err := ioutil.ReadAll(file)
+	words := strings.Split(string(rawWords), "\n")
+	return words
+}
